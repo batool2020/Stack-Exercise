7300b3db6fd259e91891cf9b8a0b4954954b4941
Batool Alsualibi <batool.alsulaibi@gmail.com>
2022-07-29 01:47:09 +0300

    commit the stack exercise solution

---
+++ Stack Exercise/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StackExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StackStrings();
+            Console.ReadLine();
+
+
+        }
+
+        private static void StackStrings()
+        {
+            var stack = new Stack<string>();
+            stack.Push("Jerusalme");
+            stack.Push("Ramallah");
+            stack.Push("Hebron");
+            stack.Push("Nablus");
+
+            stack.Peak();
+
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+
+            stack.Peak();
+
+            stack.Clear();
+
+            stack.Print();
+
+
+                }
+    }
+}
---
+++ Stack Exercise/Stack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace StackExercise
+{ 
+public class Stack<T>
+{
+	private T[] elements;
+	private int currentIndex = -1;
+
+		public Stack() => elements = new T[10]; 
+		// the simple stack class will work with only 10 items  _item: field
+													// => expression body instead of {}
+
+
+		// push items
+		public void Push(T element)
+	{
+		elements[++currentIndex] = element;
+	}
+
+		// Cou t method to calculate the number of elements in the stack
+		public int Count() { 
+			return (currentIndex + 1); 
+		}
+
+	// remove the top element of the stack and retrurn it. 
+	public T Pop()
+	{
+		return elements[currentIndex--];
+	}
+
+	public void Peak()
+	{
+		if (currentIndex == -1)
+			Console.WriteLine("no elements in the stack");
+		else
+			Console.WriteLine(elements[currentIndex]);
+	}
+
+	// Print out the stack objects
+	public void Print()
+	{
+		while (currentIndex != -1)
+			Console.WriteLine(elements[currentIndex]);
+		currentIndex--;
+	}
+
+	public void Clear()
+	{
+		while (currentIndex != -1)
+			currentIndex--;
+	}
+}
+}
